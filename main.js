935ff9cca84a64f762ba75f0a4645215d3d06e05
miguel3107 <108474194+miguel3107@users.noreply.github.com>
2024-08-30 10:50:47 -0300

    29/08

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const caixaPrincipal = document.querySelectorAll("caixa-primcipal")
-const caixaPerguntas = document.querySelectorAll("caixa-perguntas")
-const caixaAltrnativas= document.querySelectorAll("caixa-alternativas")
-const caixaResultaso = document.querySelectorAll("caixa-resultado")
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -71,3 +71,44 @@ const perguntas = [
         ]
     },
 ];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("clik",() => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+
+function respostaSelecionada(opcaoSelecionada){
+     const afirmacoes = opcaoSelecionada.afirmacao;
+     historiaFinal += afirmacoes + " ";
+     atual++;
+     mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
